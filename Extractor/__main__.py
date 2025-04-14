a254ad8eee143c4865a322ed608387417eacc147
aarav669281 <aaravmishra6691@gmail.com>
2025-04-14 11:25:30 +0530

    Update __main__.py

--- Extractor/__main__.py
+++ Extractor/__main__.py
@@ -3,10 +3,7 @@ import importlib
 from pyrogram import idle
 from Extractor.modules import ALL_MODULES
 
- 
-
 loop = asyncio.get_event_loop()
-
 
 async def sumit_boot():
     for all_module in ALL_MODULES:
@@ -16,6 +13,5 @@ async def sumit_boot():
     await idle()
     print("» ɢᴏᴏᴅ ʙʏᴇ ! sᴛᴏᴘᴘɪɴɢ ʙᴏᴛ.")
 
-
-if __name__ == "__main__":
+if name == "main":
     loop.run_until_complete(sumit_boot())
