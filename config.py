7a4b529283033a9c666d6af7c026e8853c1ff380
aarav669281 <aaravmishra6691@gmail.com>
2025-04-14 11:08:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", "26451206"))
-API_HASH = getenv("API_HASH", "32984406271d6f3945bb536671b143a7")
-BOT_TOKEN = getenv("BOT_TOKEN", "7279922550:AAF2-Tf6vpWafJgdVswQXoBzpjho7GaJhYI")
-OWNER_ID = int(getenv("OWNER_ID", "6830450483"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6830450483").split()))
+API_ID = int(getenv("API_ID", "25624473"))
+API_HASH = getenv("API_HASH", "f9064b91dc1331fe9cd614a34eb37de0")
+BOT_TOKEN = getenv("BOT_TOKEN", "7503125213:AAFsRIHoAwlQ3R-2_dh05vma4AlJGYknEKg")
+OWNER_ID = int(getenv("OWNER_ID", "7507228967"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7507228967").split()))
 MONGO_URL = getenv("MONGO_DB", "mongodb+srv://daxxop:daxxop@daxxop.dg3umlc.mongodb.net/?retryWrites=true&w=majority")
 
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002204710817"))
-PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002204710817"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002642786656"))
+PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002642786656"))
 
 """
 #
